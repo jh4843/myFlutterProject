432123469239825c256507ab66f033894bce44e9
jh4843 <creamboy1@naver.com>
2023-03-23 22:28:48 +0900

    update Variable
    update Variable

---
+++ 02_variable/main.dart
@@ -0,0 +1,66 @@
+void main() {
+  // ############################################################
+  // 01_values
+  // ############################################################
+  String strName = "재혁";
+  int nAge = 1;
+
+// ############################################################
+  // 02_dynamic or var
+  // ############################################################
+  dynamic vVal; // = var vVal
+
+  vVal = "요금";
+  vVal = 500;
+
+  print(strName);
+  print(nAge.toString());
+  print(vVal);
+
+  if (strName is String) {
+    print("strName is String!!");
+  }
+
+  if (vVal is int) {
+    print("vVal is int");
+  }
+
+  vVal = "Test Text";
+
+  if (vVal is String) {
+    print("vVal is string");
+  }
+
+  // ############################################################
+  // 03_nullable
+  // ############################################################
+  String? canNull = "Jin";
+  canNull = null;
+
+  if (canNull != null) {
+    canNull.isNotEmpty;
+  } else {
+    print(canNull);
+  }
+
+  // ############################################################
+  // 04_final
+  // ############################################################
+  final String constWorld = 'a';
+  //constWorld = 'b'; // fail
+
+  // ############################################################
+  // 05_late
+  // ############################################################
+  late final String lateWorld;
+  print(constWorld);
+  //print(lateWorld); // fail can not print the value before assign value;
+  lateWorld = 'c';
+  //lateWorld = 'd'; // fail
+
+  // ############################################################
+  // 06_const
+  // ############################################################
+  const max_price = 5000;
+  // const tt = fetchAPI() // fail because when compile, compiler can not know the type of the variable. so use final at that time.
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Overview
+
+my Flutter Projects
